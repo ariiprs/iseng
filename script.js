5a8c1b8814b6c5d2f534c68358bb2babea3fbdc3
ariiprs <111981817+ariiprs@users.noreply.github.com>
2022-12-15 10:19:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// var nama = 'ari',
+//     umur = 18,
+//     lulus = true;
+
+// console.log('Hello World');
+
+// console.log('aku ' + nama + ' aku adalah seorang programmer berusia ' + umur );
+
+// alert('hello world!');
+// alert('ikou');
+// alert('oke!');
+
+
+/*
+kamu itu cantik banget
+tapi gatau kalo lagi marah xixixiix
+*/
+
+
+//prompt('masukan nama');
+
+var name = prompt("Isi nama lengkap kalian");
+
+alert('selamat pagi ' + name)
+
+
+// console.log(name); 
+ 
